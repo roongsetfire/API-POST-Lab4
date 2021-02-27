8750940e539097e98c28b67cfe3a81c59d02777a
roong <roongsetfire@gmail.com>
2021-02-27 15:44:01 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PostListComponent } from './post-list/post-list.component';
 import { HttpClientModule } from '@angular/common/http';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
     PostListComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
-  ],
+    BrowserModule, HttpClientModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
